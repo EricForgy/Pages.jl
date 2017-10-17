7cd556504e09b3117509e2b6e8d1b3e9f19afb9f
Eric Forgy <Eric.Forgy@coherentasia.com>
2017-10-17 16:21:21 +0700

    Run examples offline.

--- examples/plotly.jl
+++ examples/plotly.jl
@@ -2,6 +2,14 @@ using Pages, PlotlyJS
 
 Pages.start()
 
+Endpoint("/libs/d3/4.2.1/d3.min.js") do request::Request
+    readstring(joinpath(dirname(@__FILE__),"libs","d3","v4.2.1","d3.min.js"))
+end
+
+Endpoint("/libs/plotly/1.16.1/plotly.min.js") do request::Request
+    readstring(joinpath(dirname(@__FILE__),"libs","plotly","v1.16.1","plotly.min.js"))
+end
+
 Endpoint("/examples/plot.ly") do request::Request
     readstring(joinpath(dirname(@__FILE__),"PagesJL.html"))
 end
